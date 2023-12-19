1afec1a2f3019a6c30693d1056d58a0e10191249
Slouj <103979007+Slouj@users.noreply.github.com>
2023-12-18 22:47:14 -0700

    Add files via upload

---
+++ finalProject/JSscript.js
@@ -0,0 +1,43 @@
+ScrollOut({
+    cssProps: true,
+    threshold: 0.2
+  });
+  
+  function openWindow(evt, windowName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+    }
+  
+    document.getElementById(windowName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+  }
+  
+  function closeWindow(windowName) {
+    document.getElementById(windowName).style.display = "none";
+    
+    var tabButton = document.querySelector('.tablinks[data-window="' + windowName + '"]');
+    if (tabButton) {
+      tabButton.classList.remove("active");
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    var tablinks = document.getElementsByClassName('tablinks');
+    for (var i = 0; i < tablinks.length; i++) {
+      tablinks[i].addEventListener('click', function () {
+      
+        var gridCells = document.querySelectorAll('.layout > div');
+        gridCells.forEach(function (cell) {
+          cell.classList.toggle('collapsed');
+        });
+      });
+    }
+  });
+  
+ 
